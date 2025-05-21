20e5458eeeb35169b3d510bdadacc818d13188bd
SexyMuscularMan <123540710+SexyMuscularMan@users.noreply.github.com>
2025-05-21 20:00:54 +0200

    version 1.3
    fixed language errors

--- main.cpp
+++ main.cpp
@@ -10,12 +10,16 @@ using namespace std;
 class Cave {
 public:
     string name;
-    int gold, level;
+    int reward, level;
     vector<enemy> enemies;
     Cave(int level)
-        : name(autoName()), gold()
-    {
+        : name(autoName()), reward(){}
 
+    //funkion der lægger enemy til enemies tager input af enemien og hvor mange der er (antal er int antal gange 1.5*lvl eller s¨ådan noget pis)
+    void addEnemy(enemy enemy, int number){
+        for (int i = 0; i < number; i++){
+        enemies.push_back(enemy);
+        }
     }
     string autoName(){ //generate name for cave
         srand(time(0)); //så rand() faktisk er random
@@ -23,22 +27,72 @@ public:
         string nouns[10] = {"Grotto","Hollow","Chasm","Cavern","Abyss","Vault","Sanctum","Catacomb","Labyrinth","Crypt"};
         return verbs[rand() % 11] + " " + nouns[rand() % 11];
     }
+
 };
-void fightIndividually(const vector<enemy> enemies, Hero hero){
+void fightIndividually(Hero& hero) {
     int choice;
+    const vector<enemy> enemies = {
+        {"Hest",               4, 1,  100},
+        {"Weak Goblin",        4, 2,  200},
+        {"Strong Goblin",      8, 3,  400},
+        {"Stronger Goblin",   10, 4,  500},
+        {"Den staerkeste Goblin",15,5,  800},
+        {"Abe Kongen",        30, 5, 1000},
+        {"Enhjoorning",         5, 8, 1500}
+    };
     cout << "choose an enemy to fight!" <<endl;
     for (int i = 1; i < enemies.size()+1; i++){
         cout << i << ": " << enemies[i-1].name << " hp: " <<enemies[i-1].hp << " damage: " << enemies[i-1].damage << endl;
     }
     cin >> choice;
+    if (choice > enemies.size() || choice < 1){
+        std::cout << "Invalid choice!\n";
+        return;
+    }
     cout << endl << "you have chosen to fight: " << enemies[choice-1].name << endl << "let the battle commence!" <<endl <<endl;
     if(Battle(hero, enemies[choice-1])){
         hero.xp += enemies[choice-1].xpReward;
         hero.tryLevelUp();
     }
 }
-void enterCave(const vector<enemy> enemies, Hero hero){
+void enterCave(Hero& hero){
+    int choice;
+    cout << "Choose to enter one of the following caves:\n1: goblin cave [easy]\n2: spider cave [medium]\n3: skeleton cave [hard]\n4: dragon cave [hardest]\n";
+    cin >> choice;
+    if (choice > 4 || choice < 1){
+        std::cout << "Invalid choice!\n";
+        enterCave(hero);
+    }
 
+    Cave cave(hero.level);
+    switch(choice) {
+    case 1: { //goblin cave
+        enemy weakGoblin({"Weak Goblin", 4+(hero.level/3), 2+(hero.level/4),  200*hero.level});
+        cave.addEnemy(weakGoblin,2+(hero.level/3));
+        enemy strongGoblin({"Strong Goblin", 8+(hero.level/3), 3+(hero.level/4),  400*hero.level});
+        cave.addEnemy(strongGoblin,1+(hero.level/2));
+        enemy strongerGoblin({"Stronger Goblin", 10+(hero.level/2), 4+(hero.level/3),  500*hero.level});
+        cave.addEnemy(strongerGoblin,(hero.level/3)); //one for every 3 levels
+        enemy strongestGoblin({"Strongest Goblin", 20,5,1000});
+        cave.addEnemy(strongestGoblin,(hero.level/10)); //unlocks at level 10
+        break;
+    }
+    case 2: { //spider cave
+
+        break;
+     }
+    case 3: { //skeleton cave
+
+         break;
+     }
+    case 4: { //dragon cave
+
+        break;
+     }
+    default:
+        std::cout << "Invalid choice!\n";
+        return;
+    }
 
 }
 void chooseWeapon(){
@@ -74,18 +128,10 @@ void game(){
     cout << "A hero named " << hero.name << " has arrived!" <<endl;
     hero.showStats();
 
-    const vector<enemy> enemies = {
-        {"Hest",               4, 1,  100},
-        {"Weak Goblin",        4, 2,  200},
-        {"Strong Goblin",      8, 3,  400},
-        {"Stronger Goblin",   10, 4,  500},
-        {"Den staerkeste Goblin",15,5,  800},
-        {"Abe Kongen",        30, 5, 1000},
-        {"Enhjoorning",         5, 8, 1500}
-    };
+
 
     while(true){
-        cout << "do you want to save and exit your game [0], fight monsters individually? [1] or enter a cave and fight many monsters? [2]" <<endl;
+        cout << "do you want to save and exit your game [0]\nfight monsters individually? [1]\nenter a cave and fight many monsters? [2]" <<endl;
         // cin.ignore();
         cin >> choice;
 
@@ -93,15 +139,15 @@ void game(){
         case 0:
             hero.showStats();
             hero.saveCharacter();
-            game();
+            return;
         case 1:
-            fightIndividually(enemies, hero);
+            fightIndividually(hero);
             break;
         case 2:
-            //enterCave(enemies, hero);
+            enterCave(hero);
             break;
         default:
-            cout << "ugyldigt valg" << endl;
+            cout << "Invalid choice!\n" << endl;
         }
 
 
